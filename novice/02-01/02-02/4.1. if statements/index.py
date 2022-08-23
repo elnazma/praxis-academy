08bb6ed127e3deb690f69fd44916e33144290477
nzlxxmld <111473569+nzlxxmld@users.noreply.github.com>
2022-08-22 21:57:14 -0700

    Add files via upload

---
+++ novice/02-01/02-02/4.1. if statements/index.py
@@ -0,0 +1,13 @@
+#print("Annyeonghaseyo yeorobun")
+x = int(input("Silahkan Masukkan Sebuah Angka: "))
+if x < 0:
+    x = 0
+    print("Replace dengan angka nol")
+# elif x == 0:
+#     print("Nol")
+# elif x == 1:
+#     print("Jomblo")
+else:
+    print("Lainnya")
+
+print(x)
